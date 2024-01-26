50491dda79d6307536e64ce3c8f78aeef51f9ada
Cynthia García González <cynthia@procoding.io>
2024-01-26 16:41:10 +0100

    Cambios de diseño

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-// Puedes agregar scripts JavaScript aquí si es necesario
-console.log('Hello from script.js!');
+const animatedBox = document.querySelector('.animated-box');
+
+animatedBox.addEventListener('mouseover', () => {
+	animatedBox.style.opacity = 1;
+});
+
+animatedBox.addEventListener('mouseout', () => {
+	animatedBox.style.opacity = 0;
+});
